38b68dac1b7a5bbe6de288d862f956f2512e87f1
Sujia2019 <184500237@qq.com>
2021-12-20 22:58:38 +0800

    update DFS

--- data_struct/graph/graph.cpp
+++ data_struct/graph/graph.cpp
@@ -58,7 +58,7 @@ typedef struct BIAN{
 }BIAN;
 
 // BFS
-// bool visited[MaxVertexNum];      // 访问标记数组
+bool visited[MaxVertexNum];      // 访问标记数组
 // void BFSTraverse(Graph G){   
 //     for(i=0;i<G.vexNum;++i){
 //          visited[i]=FALSE;       // 访问标记数组初始化
@@ -83,4 +83,21 @@ typedef struct BIAN{
 //              }
 //         }
 //     }
+// }
+
+// DFS
+// void DFSTraverse(Graph G){           // 对图G进行深度优先遍历
+//     for(v=0;v<G.vexNum;++v)
+//         visited[v]=FALSE;            // 初始化已访问标记数据
+//     for(v=0;v<G.vexNum;++v)          // 从v=0开始遍历
+//         if(!visited[v])
+//             DFS(G,v);
+// }
+// void DFS(Graph G,int v){             // 从顶点v出发，深度优先遍历图G
+//     visit(v);
+//     visited[v]=TRUE;                 // 设已访问标记
+//     for(w=FirstNeighbor(G,v);w>=0;w=NextNeighbor(G,v,w))
+//         if(!visited[w]){             // w为v的尚未访问的邻接顶点
+//             DFS(G,w);
+//         }   
 // }
